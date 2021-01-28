be57f3854b458401e2c6db0c9e82d27e4c42cf1c
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2021-01-28 16:05:49 -0300

    Aula 1.6 - Páginas mais dinâmicas com AJAX

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -47,7 +47,7 @@ public class LivroBean {
 	public void gravarAutor() {
 		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
 		this.livro.adicionaAutor(autor);
-		System.out.println("Livro escrito por: " + autor);
+		System.out.println("Escrito por: " + autor.getNome());
 	}
 	
 	public void gravar() {
