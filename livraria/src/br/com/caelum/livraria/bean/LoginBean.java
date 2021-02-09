c02b64f84c0be3e65f0c83ef4e345ede8f768ca9
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2021-02-09 14:04:33 -0300

    Aula 1.13 - O escopo Flash

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,11 +1,13 @@
 package br.com.caelum.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDAO;
 import br.com.caelum.livraria.modelo.Usuario;
+import br.com.caelum.livraria.util.ForwardView;
 import br.com.caelum.livraria.util.RedirectView;
 
 @ManagedBean
@@ -20,13 +22,18 @@ public class LoginBean {
 	
 	public RedirectView efetuaLogin() {
 		System.out.println("Fazendo login do usuário " + this.usuario.getEmail());
+		
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDAO().existe(this.usuario);
 		if(existe) {
-			FacesContext context = FacesContext.getCurrentInstance();
 			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return new RedirectView("livro");
 		}
-		return null;
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);;
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+		
+		return new RedirectView("login");
 	}
 	
 	public RedirectView deslogar() {
